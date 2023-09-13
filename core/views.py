c7ccb207622dda42472c379c8c5ec129c1d456bc
leoaguiar07 <leorodriguesaguiar@gmail.com>
2023-09-13 12:15:35 -0300

    Incluído template para inserção de eventos da agenda, incluído gitignore para ignorar os arquivos compilados

--- core/views.py
+++ core/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, HttpResponse,redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login,logout
-from core.models import Evento
 from django.contrib import messages
+from core.models import Evento
+
 
 
 # Create your views here.
@@ -18,6 +19,27 @@ def logout_user(request):
     logout(request)
     return redirect ('/')
 
+#verificar se está logado
+@login_required(login_url='/login/')
+def evento(request):
+    return render(request, 'evento.html')
+
+#verificar se está logado
+@login_required(login_url='/login/')
+#Criar evento
+def submit_evento(request):
+    if request.POST:
+        titulo = request.POST.get('titulo')
+        data_evento = request.POST.get('data_evento')
+        descricao = request.POST.get('descricao')
+        usuario = request.user
+        Evento.objects.create(titulo=titulo,
+                              data_evento = data_evento,
+                              descricao=descricao,
+                              usuario=usuario)
+        return redirect('/')
+
+#Validar LOGIN
 def submit_login(request):
     if request.POST:
         username= request.POST.get('username')
